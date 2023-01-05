f685a8096a7d5fd50cd4fff4eb12d5d4c21307b7
Jesse Stricker <git@jessestricker.de>
2023-01-05 15:08:54 +0100

    feat: wait for process to terminate, forward exit code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <optional>
 #include <string>
 #include <string_view>
 
@@ -13,54 +14,94 @@ TRACELOGGING_DEFINE_PROVIDER(logging_provider, "JesseStricker.run_hidden",
                               0xc5, 0xf2, 0xa8, 0x52, 0x60));
 
 namespace run_hidden {
-  /// Start a new process with the given command line.
-  /// This function does __not__ wait until the new process exists.
-  void start_process(std::wstring_view cmd_line) {
+  /// Wait for a process to end and log the exit code, if available.
+  std::optional<int> wait_for_process(HANDLE process) {
+    if (WaitForSingleObject(process, INFINITE) == WAIT_FAILED) {
+      const auto error_code = GetLastError();
+      TraceLoggingWrite(logging_provider, "WaitForSingleObjectFailed",
+                        TraceLoggingLevel(WINEVENT_LEVEL_ERROR),
+                        TraceLoggingWinError(error_code));
+      return std::nullopt;
+    }
+
+    // get exit code
+    auto exit_code = DWORD{};
+    if (!GetExitCodeProcess(process, &exit_code)) {
+      const auto error_code = GetLastError();
+      TraceLoggingWrite(logging_provider, "WaitForSingleObjectFailed",
+                        TraceLoggingLevel(WINEVENT_LEVEL_ERROR),
+                        TraceLoggingWinError(error_code));
+      TraceLoggingWrite(logging_provider, "ProcessTerminated",
+                        TraceLoggingLevel(WINEVENT_LEVEL_VERBOSE));
+      return std::nullopt;
+    }
+    if (exit_code == STILL_ACTIVE) {
+      // unreachable, since we already waited for the process to terminate
+      TraceLoggingWrite(
+          logging_provider, "InvalidReturnValue",
+          TraceLoggingLevel(WINEVENT_LEVEL_WARNING),
+          TraceLoggingValue("GetExitCodeProcess", "FunctionName"));
+      TraceLoggingWrite(logging_provider, "ProcessTerminated",
+                        TraceLoggingLevel(WINEVENT_LEVEL_VERBOSE));
+      return std::nullopt;
+    }
+
+    // log with exit code
+    TraceLoggingWrite(logging_provider, "ProcessTerminated",
+                      TraceLoggingLevel(WINEVENT_LEVEL_VERBOSE),
+                      TraceLoggingValue(exit_code, "ExitCode"));
+    return {static_cast<int>(exit_code)};
+  }
+
+  /// Start a new process with the given command line and wait until it
+  /// terminates.
+  int run_process(std::wstring_view cmd_line) {
     // create a modifiable, null-terminated copy of the command line
     auto cmd_line_copy = std::wstring{cmd_line};
     cmd_line_copy.push_back('\0');
 
+    // create process
     auto startup_info = STARTUPINFO{.cb = sizeof(STARTUPINFO)};
     auto process_info = PROCESS_INFORMATION{};
-    const auto result = CreateProcess(nullptr,               // application name
-                                      cmd_line_copy.data(),  // command line
-                                      nullptr,           // process attributes
-                                      nullptr,           // thread attributes
-                                      false,             // inherit handles
-                                      CREATE_NO_WINDOW,  // creation flags
-                                      nullptr,           // environment
-                                      nullptr,           // current directory
-                                      &startup_info, &process_info);
-
-    // handle process creation error
-    if (result == 0) {
+    if (!CreateProcess(nullptr,               // application name
+                       cmd_line_copy.data(),  // command line
+                       nullptr,               // process attributes
+                       nullptr,               // thread attributes
+                       false,                 // inherit handles
+                       CREATE_NO_WINDOW,      // creation flags
+                       nullptr,               // environment
+                       nullptr,               // current directory
+                       &startup_info, &process_info)) {
       const auto error_code = GetLastError();
       TraceLoggingWrite(logging_provider, "CreateProcessFailed",
                         TraceLoggingLevel(WINEVENT_LEVEL_ERROR),
                         TraceLoggingWinError(error_code));
-      return;
+      return 1;
     }
 
     TraceLoggingWrite(logging_provider, "ProcessCreated",
                       TraceLoggingLevel(WINEVENT_LEVEL_VERBOSE),
                       TraceLoggingWideString(cmd_line.data(), "CommandLine"));
 
-    WaitForInputIdle(process_info.hProcess, INFINITE);
+    // wait until process terminates
+    const auto exit_code = wait_for_process(process_info.hProcess);
+
+    // close process handles
     CloseHandle(process_info.hProcess);
     CloseHandle(process_info.hThread);
+
+    return exit_code.value_or(1);
   }
 
-  void main(std::wstring_view cmd_line) {
+  int main(std::wstring_view cmd_line) {
     TraceLoggingRegister(logging_provider);
-
-    start_process(cmd_line);
-
+    const auto exit_code = run_process(cmd_line);
     TraceLoggingUnregister(logging_provider);
+    return exit_code;
   }
 }
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance,
                     wchar_t* cmd_line, int cmd_show) {
-  run_hidden::main(cmd_line);
-  return 0;
+  return run_hidden::main(cmd_line);
 }
